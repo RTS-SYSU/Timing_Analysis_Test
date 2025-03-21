257090934e8bfb34f8bc8d5f0508b755219ab3dc
niaov <1254567563@qq.com>
2025-03-21 11:43:25 +0000

    updata ourmethod

--- include/Util/Ourmethod.h
+++ include/Util/Ourmethod.h
@@ -15,6 +15,7 @@ struct BlockInfo {
   unsigned exe_cnt; //执行次数
   int age;     // 年龄，非持久性块使用
   int cs_size; // 冲突集，持久性块专用
+  int cap;  // 持久性块逐出最小cache数
   TimingAnalysisPass::dom::cache::Classification cl;
   BlockInfo() : cs_size(INT_MAX) {}
   BlockInfo(unsigned addr, unsigned cnt, int a,
@@ -23,10 +24,20 @@ struct BlockInfo {
     this->address = addr;
     this->exe_cnt = cnt;
     this->age = a;
-    this->cs_size = sz;
+    this->cs_size = sz;   // 这是一个注释
     this->cl = c;
+    this->cap = std::max(L2assoc-cs_size,0);
   }
+
+  bool operator <(const BlockInfo& a) const{  // 辅助函数，用于set<BlockInfo>
+    if (address != a.address) return address < a.address;
+    else if (age != a.age)  return age < a.age;
+    else if (exe_cnt != a.exe_cnt)  return exe_cnt < a.exe_cnt;
+    return cap < a.cap;          
+  }
+
 };
+
 
 class OurM {
 public:
@@ -34,6 +45,193 @@ public:
   typedef std::vector<UR> Ceop;
   typedef std::vector<Ceop> Ceops;
   std::map<std::string, Ceops> f2ceops;
+
+  unsigned getIndex(unsigned addr){ 
+    int blockOffset = log2(L2linesize), setOffset = log2(NN_SET);
+    if ((1 << blockOffset) < L2linesize) blockOffset++;
+    if ((1 << setOffset) < NN_SET) setOffset++;
+
+    return (addr >> blockOffset) & ((1 << setOffset) - 1);
+  }
+  unsigned getTag(unsigned addr){
+    int blockOffset = log2(L2linesize), setOffset = log2(NN_SET);
+    if ((1 << blockOffset) < L2linesize) blockOffset++;
+    if ((1 << setOffset) < NN_SET) setOffset++;
+
+    return addr >> blockOffset >> setOffset;
+  }
+
+  struct CR{
+    std::vector<BlockInfo> PersistentBlock;
+    std::vector<BlockInfo> NotPersistBlock;
+
+    // 非持久性块在给定二进制状态State后，经过指定UR后被逐出的最大次数
+    int OutNumber_NotPersistBlock(int& State, UR ur){  
+      std::map<unsigned,std::set<unsigned>> setInfo;
+      for(auto block:ur){     // 先求出UR中组的情况
+          unsigned addr = block.address, index = getIndex(addr), tag = getTag(addr);
+          setInfo.insert(tag);
+      }
+
+      int ans=0;
+      for(int i=0;i<NotPersistBlock.size();++i){      // 遍历剩余的非持久性块
+          if(State & (1<<i))  continue;
+          BlockInfo block = NotPersistBlock[i];
+          int age=block.age;
+          unsigned addr=block.address, index = getIndex(addr), tag = getTag(addr);
+          
+          if(setInfo.count(tag))  continue; // 若组里面已经有addr所在的缓存行Tag则跳过
+          else if(age + setInfo[index].size() > L2assoc)
+                ++ans, State |= (1<<i);   
+      }
+      return ans;
+    }
+
+    // 持久性块在指定UR中被逐出次数
+    std::vector<int> OutNumber_PersistentBlock(UR ur){
+      std::vector<int> anss;
+      for(auto block:PersistentBlock){
+          int sum=0, L=0, R=0, ans=0, cap=block.cap, cnt=block.cnt;
+          for(auto block:ur)
+              sum+=block.cnt;
+          R=std::min(cnt, sum/cap);   ///////////////////////////////////////////////////////
+          while(L<=R){
+              int mid=L+R>>1, temp=0;
+              for(auto block:ur)
+                  temp+=std::min(mid, block.cnt);
+              if(temp>=cap*mid)   L=mid, ans=mid;
+              else    R=mid;
+          }
+          anss.emplace_back(ans);
+      }
+      return anss;
+    }
+
+    // 根据持久性块被逐出的情况更新Cr并对令下一个UR继承遗产  //////////////////////////////////////////////////////
+    void updataCrUr(std::vector<int>PersistOut, UR ur, UR &NextUr){
+      int mxOut = 0;
+      for(int i=0;i<PersistentBlock.size();++i){
+          int num = PersistOut[i];
+          mxOut = std::max(mxOut, num);   // 记录cr中单个持久性块被逐出的最大次数（即ur中block理论最高逐出次数）
+          PersistentBlock[i].cnt -= num;
+      }
+      for(auto block:ur){
+          if(block.cnt>mxOut)   // 先不考虑下一个ur和上一个ur有相同address的block的情况
+            block.cnt=1, NextUr.push_back(block);
+      }
+    }
+  };
+
+  struct CRs{
+    std::vector<CR> crs;
+
+    // 每条路径都需要初始化一次CRs
+    CRs(Ceop ceop){   
+      // 记录每个UR中包含的区间左端点和区间右端点（非持久性块）
+      std::vector<std::vector<BlockInfo>> leftNodesInURs, rightNodesInURs;
+      // 记录每个UR拥有的持久性块（注意判定持久性块的方式 cnt cap）
+      std::vector<BlockInfo> PersistBlockInURs;
+      leftNodesInURs.resize(ceop.size()), rightNodesInURs.resize(ceop.size());
+      PersistBlockInURs.resize(ceop.size());
+      
+      std::map<unsigned, int> LastURforBlock; // Block addr 到最近 UR 位置的映射
+      for (int i = 0; i < ceop.size(); ++i) {
+          for (auto block : ceop[i]) { 
+              int cnt = block.cnt, age = block.age, cap=block.cap;
+              unsigned addr=block.address;
+
+              // 持久性块单独处理（要求不存在cap <= 0的块）   ///////////////////////////////////
+              if(cnt>1)     PersistBlockInURs.push_back(block);
+              // 检查是否成对出现且不在缓存外
+              else if (LastURforBlock.count(addr) && LastURforBlock[addr] + 1 < i && age >= 0) { 
+                  int leftUR = LastURforBlock[addr] + 1, rightUR = i;   
+                  leftNodesInURs[leftUR].push_back(block);
+                  rightNodesInURs[rightUR].push_back(block); 
+              }
+              LastURforBlock[addr] = i;
+          }
+      }
+
+      // 遍历UR开始划分CR
+      std::set<BlockInfo> NotPersistBlocks;
+      for (int i = 0; i < ceop.size(); ++i) {
+          // 若UR存在区间端点需要生成新的CR
+          if (!leftNodesInURs[i].empty() || !rightNodesInURs[i].empty() || !PersistBlockInURs.empty()) {  
+              CR cr;
+              cr.NotPersistBlock.assign(NotPersistBlocks.begin(), NotPersistBlocks.end());
+              cr.PersistentBlock.assign(PersistBlockInURs.begin(), PersistBlockInURs.end());
+              crs.push_back(cr);   // 注意cr内元素默认升序
+
+              // 更新block id
+              for (auto &p : leftNodesInURs[i]) NotPersistBlocks.insert(p);
+              for (auto &p : rightNodesInURs[i]) NotPersistBlocks.erase(p);
+          }
+      }
+
+    }
+
+    int DpMaxOutNumber(Ceop Urs){
+      // dp[k][i][j] = 第K个CR在二进制状态i的条件下，最远分配到干扰任务的第j个UR的最大逐出次数，由于k-2后数据没有意义，因此只记录上一个dp
+      std::vector<std::vector<int>> lastDp;    
+      std::vector<int> Urs_Dp_size(Urs.size(), 0);     // 干扰任务URs的长度，作为lastDp的基本单位
+      lastDp.push_back(Urs_Dp_size);     // 初始化
+      std::vector<BlockInfo> lastCRBlock;   // 记录上一个CR中有什么block id
+
+      for(auto cr:crs){
+          std::vector<std::vector<int>> curDp;        // 本轮迭代计算的结果
+          int NumNotPersistentBlock = cr.NotPersistBlock.size();
+          curDp.resize(1<<NumNotPersistentBlock, Urs_Dp_size);
+
+          // 枚举二进制状态，获取上一个CR的某个block是否被逐出 
+          for (int prevState = 0; prevState < lastDp.size(); ++prevState) { 
+              int inheritedOut = 0;   // 当前CR初始逐出的block状态
+
+              // 计算继承的Block逐出状态   // 需要处理CR中的非持久块
+              for (int bidIdx = 0; bidIdx < lastCRBlock.size(); ++bidIdx) {
+                  if (!(prevState & (1 << bidIdx))) continue;
+                  unsigned addr = lastCRBlock[bidIdx].address;
+                  
+                  // 查找当前CR中对应的block
+                  for(int bid = 0; bid < cr.NotPersistBlock.size();++bid)
+                    if(cr.NotPersistBlock[bid].address == addr){
+                      inheritedOut[bid] |= (1<<bid);
+                      break;
+                    }
+              }
+
+              CR cr2 = cr;    // 由于cr、Urs都涉及到修改操作，因此需要开一个副本
+              Ceop Urs2 = Urs; 
+              // 遍历干扰任务起始位置
+              for (int startUR = 0; startUR < Urs.size(); ++startUR) {
+                  int curOut = inheritedOut, totalOut = 0;
+
+                  // 模拟干扰任务执行
+                  for (int curUR = startUR; curUR < Urs.urs.size(); ++curUR) {
+                      totalOut += cr2.OutNumber_NotPersistBlock(curOut, Urs.urs[curUR]);
+                      std::vector<int> PersistOut = cr2.OutNumber_PersistentBlock(Urs.urs[curUR]);
+                      for(auto val:PersistOut)    totalOut += val;
+                      if(curUR + 1 < Urs.urs.size())  // 只要不是最后一个UR就需要使用遗产
+                          cr2.updataCrUr(PersistOut, Urs.urs[curUR], Urs.urs[curUR+1]);
+
+                      // 更新dp状态
+                      curDp[curOut][curUR] = std::max(curDp[curOut][curUR], 
+                                                  lastDp[prevState][startUR] + totalOut);
+                  }
+              }
+
+          }
+          lastDp.swap(curDp);
+          lastCRBlock = cr.NotPersistBlock;
+      }
+     // 查找最大逐出次数
+      int maxVal = 0;
+      for (auto& states : lastDp)
+          for (int val : states)
+              maxVal = std::max(maxVal, val);
+      return maxVal;
+    }
+  };
+
 
   OurM(Zhangmethod zm, CL_info &cl_infor) {
     // pre processing of PS block
